387adae7eedb2a208938c10d90eb8fe71c13709e
octabia <octabia_fabia@aol.com>
2017-08-22 11:52:05 +0200

    modified fhem/start.sh

--- fhem/start.sh
+++ fhem/start.sh
@@ -4,5 +4,7 @@ set -e
 cd /opt/fhem
 port=7072
 
+cp /opt/fhem/www/images/default/fhemicon_dark_cpid.png /opt/fhem/www/images/default/fhemicon_dark.png
+
 echo "Starte FHEM"
 perl fhem.pl fhem.cfg | tee /opt/fhem/log/fhem.log
